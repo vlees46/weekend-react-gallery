89af73b2ad149e2d6e262cdd639d73e61789c2fb
Victor Lees <vlees14.pa@gmail.com>
2021-11-26 10:15:06 -0500

    Added additional gallery pictures

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: '/public/images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: '/public/images/BillyGoat.jpg', description: 'Domestic GOAT - Billy goat.', likes: 0 }
+    { id: 1, path: '/public/images/Wild-Turkey.jpg', description: 'A male, or tom, Wild Turkey. Photo by RelentlessImages/Shutterstock', likes: 0 }
+    { id: 1, path: '/public/images/turkeycrossing.jpg', description: 'Photo of adult turkeys crossing a road.', likes: 0 }
+    { id: 1, path: '/public/images/alpaca-01.jpg', description: 'The alpaca is a small relative of the camel and was domesticated by Andean people for its wool.', likes: 0 }
+    { id: 1, path: '/public/images/Bactrian-camel.jpg', description: 'The Bactrian camel (Camelus bactrianus) is about 7 ft (2 m) tall at the top of the two humps;', likes: 0 }
 ];
 
 module.exports = galleryItems;
