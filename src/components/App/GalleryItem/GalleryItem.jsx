df493df9743d15f5f13167a1b45b9338a050ff85
Victor Lees <vlees14.pa@gmail.com>
2021-11-27 15:38:48 -0500

    almost working, need to see why images are not rendering to dom after adding likes button

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react';
 import '../GalleryItem/GalleryItem.css';
 
 
-function GalleryItem(){
+function GalleryItem( photo, addLike){
 
+    let [isClicked, setisClicked] = useState(false);
+    let likesDisplay;
 
+    if(photo.likes == 0){
+        likesDisplay = "No likes yet";
+    }
+    else if(photo.likes > 0) {
+        likesDisplay = photo.likes;
+    }
 
     return (
 
@@ -12,15 +20,15 @@ function GalleryItem(){
             <li key={photo.id} className="picture">
 
                 {
-                    isPhotoClicked
+                    isClicked
                         ?
-                        <div className="description" onClick={() => setIsPhotoClicked(!isPhotoClicked)}><span id="theText">{photo.description}<br/><br />{photo.location}</span></div>
+                        <div className="description" onClick={() => setisClicked(!isClicked)}><span id="theText">{photo.description}<br/><br />{photo.location}</span></div>
                         :
-                        <img className="image" width="200px" height="200" onClick={() => setIsPhotoClicked(!isPhotoClicked)} src={photo.path} />
+                        <img className="image" width="200px" height="200" onClick={() => setisClicked(!isClicked)} src={photo.path} />
                 }
 
                 <div className="likeSection">
-                    <img className="loveButton" width="35px" onClick={() => addLike(photo.id)} src="/images/thumbs-up-icon.png" />
+                    <img className="thumbsupbutton" width="35px" onClick={() => addLike(photo.id)} src="/images/thumbs-up-icon.png" />
 
                     <span id="likesNumber">{likesDisplay}</span>
                 </div>
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -4,18 +4,37 @@ import axios from 'axios';
 import GalleryItem from '../GalleryItem/GalleryItem.jsx'
 import '../GalleryList/GalleryList.css';
 
-function GalleryList({ galleryArray, getGallery }) {
+function GalleryList({ getgalleryArray, getGallery }) {
+
+    let [isClicked, setIsClicked] = useState(false);
+
+
+    //PUT - to add likes ...is this supposed to stay on the app.jsx page?
+    const addLike = (galleryID) => {
+        console.log('adding Like to photo with galleryID:', galleryID)
+        axios({
+            method: 'PUT',
+            url: 'gallery/like/' + galleryID
+        }).then(function (response) {
+            console.log('Adding like to photo with ID:', galleryID)
+            getGallery();
+        }).catch(function (error) {
+            console.log('error in CLIENT-side PUT function:', error)
+        })
+    } // end PUT function to add a like (working)
+
+
 
 return (
 
     <ul className="list">
 
-        {galleryArray.map(photo => (
+        {getgalleryArray.map(photo => (
             <GalleryItem
                 key={photo.id}
                 photo={photo}
-                isPhotoClicked={isPhotoClicked}
-                setIsPhotoClicked={setIsPhotoClicked}
+                isClicked={isClicked}
+                setIsClicked={setIsClicked}
                 addLike={addLike}
             />
         ))}
